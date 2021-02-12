6154a60cc598fd5a02405f5b598aa315023c79b3
Aho Senpai <nicosenpai380@gmail.com>
2021-02-13 10:30:20 +1100

    data.js sorting and cleanup

--- scripts/data.js
+++ scripts/data.js
@@ -1,10 +1,8 @@
 // Thanks to https://github.com/Rawrington/SkillDisplay/blob/master/src/Action.js
 const gcdOverrides = [
-	15997, //standard step
-	15998, //technical step
-	15999,
-	16000,
-	16001,
+	// DNC
+    15997, //standard step
+	15998, 15999, 16000, 16001, //technical step
 	16002, //step actions
 	16003, //standard finish
 	16004, //technical finish
@@ -14,42 +12,34 @@ const gcdOverrides = [
 	16194, //double technical finish
 	16195, //triple technical finish
 	16196, //quadruple technical finish
+    // MCH
 	7418, //flamethrower
-	16484, //kaeshi higanbana
-	16485, //kaeshi goken
-	16486, //kaeshi setsugekka
-	2259, //ten
-	18805, 
-	2261, //chi
-	18806,
-	2263, //jin
-	18807,
-	2265, //fuma shurikan
-	18873,
-	18874,
-	18875,
-	2267, //raiton
-	18877,
-	2266, //katon
-	18876,
-	2268, //hyoton
-	18878,
+    // NIN
+	2259, 18805, //ten
+	2261, 18806, //chi
+	2263, 18807, //jin
+	2265, 18873, 18874, 18875, //fuma shurikan
+	2266, 18876, //katon
+	2267, 18877, //raiton
+	2268, 18878, //hyoton
+	2269, 18879, //huton
+	2270, 18880, //doton
+	2271, 18881, //suiton
+    2272, //rabbit medium
 	16492, //hyosho ranryu
 	16491, //goka meykakku
-	2270, //doton
-	18880,
-	2269, //huton
-	18879,
-	2271, //suiton
-	18881,
-    2272, //rabbit medium
+    // SAM
     16483, //Tsubame-Gaeshi
+    16484, //kaeshi higanbana
+	16485, //kaeshi goken
+	16486, //kaeshi setsugekka
 ];
 // Thanks to https://github.com/Rawrington/SkillDisplay/blob/master/src/Action.js
 const ogcdOverrides = [
-	3559, //bard WM
+    // BRD
+    114, //bard MB
 	116, //bard AP
-	114 //bard MB
+	3559, //bard WM
 ];
 
 const globalSkillsList = [
@@ -124,15 +114,12 @@ const globalSkillsList = [
 ];
 
 const skillsBlacklist = [
-    18805, //NIN Mudras with "bad" Skill Description
-    18806,
-    18807,
-    19238, //BLU Aetherial Mimickery with "bad" Skill Desc
-    19239,
-    19240
+    18805, 18806, 18807, //NIN Mudras with "bad" Skill Description
+    19238, 19239, 19240, //BLU Aetherial Mimickery with "bad" Skill Desc
 ];
 
 const skillsWhitelist = [
+    // NIN
     2265, //Fuma
     2266, //Katon
     2267, //Raiton
@@ -143,7 +130,9 @@ const skillsWhitelist = [
     2272, //Rabbit Medium
 	16491, //goka meykakku
     16492, //hyosho ranryu
+    // MNK
     3547, //Forbidden Chacra
+    // AST
     4401, //Balance
     4402, //Arrow
     4403, //Spear
@@ -152,44 +141,37 @@ const skillsWhitelist = [
     4406, //Spire
     7444, //Lord
     7445, //Lady
+    // SAM
     7487, //Midare
     7488, //Tenka Goken
     7489, //Higanbana
     16484, //Kaeshi: Higanbana
     16485, //Kaeshi: Goken
     16486, //Kaeshi: Setsugekka
+    // DNC
     15999, //Emboite
     16000, //Entrechat
     16001, //Jete
     16002, //Pirouette
     16003, //Standard Finish
     16004, //Technical Finish
+    18073, //Ending
+    // RDM
     7527, //Enchanted Ripost
     7528, //Enchanted Zwerchhau
     7529, //Enchanted Redoublement
     7530, //Enchanted Moulinet
-    16528, //Enchanted Reprise
     7525, //Verflare
     7526, //Verfire
+    16528, //Enchanted Reprise
     16530, //Scorch
+    // SMN
+    7426, //Ruin IV
     16513, //Firebird Trance
     16514, //Fountain of Fire
     16515, //Brand of Purgatory
     16516, //Enkindle Phoenix
     16517, //Everlasting Flight
-    16156, //Jugular Rip
-    16157, //Abdomen Tear
-    16158, //Eye Gouge
-    7400, //Nastrond
-    16465, //Inner Chaos
-    16463, //Chaotic Cyclone
-    16479, //Raiden Thrust
-    16766, //MCH Detonator
-    18073, //DNC Ending
-    7426, //Ruin IV
-    23268, //BLU White Death
-    23289, //BLU Fantom Flurry (Second Hit)
-    23274, //BLU Divine Cataract (Chelonian Gate Second Hit)
     16791, //Assault I: Glittering Topaz	Action		
     16792, //Assault II: Shining Topaz	Action		
     16793, //Assault I: Downburst	Action		
@@ -200,4 +182,21 @@ const skillsWhitelist = [
     16798, //Assault II: Slipstream	Action		
     16799, //Assault I: Crimson Cyclone	Action		
     16800, //Assault II: Flaming Crush
+    // GNB
+    16156, //Jugular Rip
+    16157, //Abdomen Tear
+    16158, //Eye Gouge
+    // DRG
+    7400, //Nastrond
+    16479, //Raiden Thrust
+    // WAR
+    16463, //Chaotic Cyclone
+    16465, //Inner Chaos
+    // MCH
+    16766, //Detonator
+    // BLU
+    23268, //White Death
+    23274, //Divine Cataract (Chelonian Gate Second Hit)
+    23289, //Fantom Flurry (Second Hit)
+
 ];
