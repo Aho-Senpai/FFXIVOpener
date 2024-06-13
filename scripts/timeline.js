512247bd6ddbbfbba53e0bfe8014a14f2e8417f4
Aho Senpai <nicosenpai380@gmail.com>
2024-06-13 19:30:31 +1100

    raidbuffstimeline kinda working

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -6,9 +6,10 @@ function addToTimeline() {
         document.getElementById("SkillsTimeline").appendChild(TempImg);
     }
     if (this.classList.contains("RaidBuff")) {
-        console.log(this.id);
+        //console.log(this.id);
         let Temp = document.createElement("div");
         Temp.textContent = this.id;
+        Temp.addEventListener("mousedown", moveBuffs)
         Temp.style.backgroundColor = "red"; // TODO : change
         document.getElementById("RaidBuffsTimeline").prepend(Temp);
     }
@@ -18,4 +19,18 @@ function removeFromTimeline() {
     document.getElementById("SkillsTimeline").removeChild(this);
 }
 
-//TODO : Add ability to move raidbuffs (move right = add margin left)
+//TODO : Edit this a bit, not smooth (something like "whileMouseDown" instead of "onMouseDown" ?)
+function moveBuffs() {
+    document.onmousedown = (e) => {
+        this.classList.add("BuffDivMove");
+    }
+    document.onmousemove = (e) => {
+        if (this.classList.contains("BuffDivMove")) {
+            let x = e.pageX;
+            this.style.marginLeft = (x - 50) + "px";
+        }
+    }
+    document.onmouseup = (e) => {
+        this.classList.remove("BuffDivMove");
+    }
+}
