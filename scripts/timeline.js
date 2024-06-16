7b4d5a2884e80d75a8a14003ecd5e4899cdbeab1
Aho Senpai <nicosenpai380@gmail.com>
2024-06-17 05:56:38 +1100

    RaidBuffsTimeline resize working

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -9,27 +9,37 @@ function addToTimeline() {
         let Temp = document.createElement("div");
         Temp.textContent = this.id;
         Temp.classList.add("RaidBuffTimelineBar");
-        Temp.addEventListener("pointerdown", (e) => pointerDownBuff(e));
+        const resizeObserver = new ResizeObserver((entries) => {
+            for (const entry of entries) {
+                if (entry.target === Temp) {
+                    entry.target.classList.remove("BuffDivMove");
+                    entry.target.style.cursor = "grab";
+                }
+            }
+        })
+        Temp.addEventListener("pointerdown", (e) => pointerDownBuff(e, resizeObserver));
         Temp.addEventListener("pointermove", (e) => pointerMoveBuff(e));
-        Temp.addEventListener("pointerup", (e) => pointerUpBuff(e));
+        Temp.addEventListener("pointerup", (e) => pointerUpBuff(e, resizeObserver));
         Temp.style.backgroundColor = "red"; // TODO : change
         document.getElementById("RaidBuffsTimeline").prepend(Temp);
     }
 }
-function pointerDownBuff(e) {
+function pointerDownBuff(e, resizeObserver) {
     if (!e.target.classList.contains("RaidBuffTimelineBar")) { return; }
     e.target.classList.add("BuffDivMove");
     e.target.style.cursor = "grabbing";
+    resizeObserver.observe(e.target);
 }
 function pointerMoveBuff(e) {
     if (!e.target.classList.contains("BuffDivMove")) { return; }
     let x = e.pageX;
     e.target.style.marginLeft = (x - 50) + "px";
 }
-function pointerUpBuff(e) {
+function pointerUpBuff(e, resizeObserver) {
     if (!e.target.classList.contains("BuffDivMove")) { return; }
     e.target.classList.remove("BuffDivMove");
     e.target.style.cursor = "grab";
+    resizeObserver.unobserve(e.target);
 }
 
 function removeFromTimeline() {
