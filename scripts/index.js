fa73531eb139f133d19b58723f4b9f0f6ad9959b
Aho Senpai <nicosenpai380@gmail.com>
2024-06-20 07:49:12 +1100

    todo update + setting fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// TODO: MoxSetting = do not clear the timeline when option is selected
-
 // MEMO : Background Colors
 // function jobSelectBackgroundColor(role) {
 //     switch (role) {
@@ -22,6 +20,8 @@ function githubBtn() {
     window.open("https://github.com/Aho-Senpai/FFXIVOpener");
 }
 
+//TODO : remove sortableJS dependency and implement sorting of timeline
+//TODO: change background color of each raidbuff/selfbuff? custom edit thingy?
 //TODO : add a "under level" category in GCD/OGCD lists?
 //TODO : Ability to create custom raidbuff?
 //TODO : default opener per job?
@@ -29,3 +29,8 @@ function githubBtn() {
 //TODO : Settings menu prettier
 //TODO : Fix tooltip issue when too close to right edge
 //TODO : for share and settings "menus", make them close/hide when clicking outside the div
+//TODO : chromium fix
+
+//todo : feature request : https://discord.com/channels/277897135515762698/283058569471852546/740962443148394516
+    // ^ make each GCD smaller and smaller if in a row (low priority)
+//todo : localization
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,5 @@
 let mox = false; // if true: do not clear timeline when selecting job
+mox = document.getElementById("MoxSettingCB").checked;
 function settingsBtn() {
     const settingsDiv = document.getElementById("SettingsDiv");
     settingsDiv.style.display = getComputedStyle(settingsDiv).display === "block" ? "none" : "block";
